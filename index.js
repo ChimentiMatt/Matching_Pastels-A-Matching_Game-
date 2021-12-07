14972bb5c5fc5676c304a47a4c1fe3d441c4dab4
matthew chimenti <matthewosu@gmail.com>
2021-12-06 23:23:16 -0800

    working alpha version

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 
 const board = document.getElementById('board')
 cardImages = {
-    0: '#6b7b8e',
-    1: '#85e3ff',
-    2: '#aff8d8',
-    3: '#ffb5e8',
-    4: '#836953',
-    5: '#f67280',
-    6: '##ffc37f',
+    0: '#85e3ff',
+    1: '#ffb5e8',
+    2: '#f67280',
+    3: '#f4fffa',
+    4: '#6b7b8e',
+    5: '#836953',
+    6: '#ffc37f',
+    7: '#4D4DFF',
+    8: '#E0E722',
+    9: '#FFAD00',
+    10: '#DB3EB1',
+    11: '#44D62C',
+    12: '#cccccc',
 }
+
+let attempts = 5
+let score = 0
+let matcherPerRound = 0
 
 let card1 = ''
 let card2 = ''
@@ -22,104 +32,181 @@ let dom2 = ''
 let card1Back = ''
 let card2Back = ''
 
-let rangeVar = [...Array(10).keys()]
+let x = 4
+
+// let rangeVar = [...Array(x).keys()]
 let tempArray = []
 let tempString = ''
 
 let matchCounter = 0
 let counter = 0
 
-for (let i = 0; i < rangeVar.length; i++){
-    if (matchCounter == 2){
-        matchCounter = 0
-        counter += 1
-        // console.log('match made')
+
+
+function generateGame() {
+
+    for (let i = 0; i < [...Array(x).keys()].length; i++){
+        if (matchCounter == 2){
+            matchCounter = 0
+            counter += 1
+        }
+        tempString = '<div class="cards" id="' + counter + '" onClick="showId()"></div>'
+        tempArray.push(tempString)
+        matchCounter += 1
+        // board.innerHTML += '<div class="cards" id='+counter+'></div>'
     }
-    tempString = '<div class="cards" id="' + counter + '" onClick="showId()"></div>'
+
+    let shuffledArray = tempArray.sort((a, b) => 0.5 - Math.random());
+    // console.log(shuffledArray)
+
+    for (let i = 0; i < [...Array(x).keys()].length; i++){
+        board.innerHTML += shuffledArray[i]
+    }
+    console.log(board)
+}
+
+//turns all visibility hidden dives to also display none. This way it isnt confusing in game was cards stay in place, but after there isnt weird gaps
+function clearnUpHiddenDivs() {
+    let cardsArray = document.querySelectorAll('.cards')
+
+    cardsArray.forEach(item => {
+        if (item.style.visibility == 'hidden'){
+            item.style.display = 'none'
+            }   
+        })
+}
+
+function newItems() {
+    // alert(1)
+    counter += 1
+    tempString = '<div class="cards" id="'+counter+'" onClick="showId()"></div>'
     tempArray.push(tempString)
-    matchCounter += 1
-    // board.innerHTML += '<div class="cards" id='+counter+'></div>'
+    tempString = '<div class="cards" id="'+counter+'" onClick="showId()"></div>'
+    tempArray.push(tempString)
 }
 
+async function addMoreItems() {
 
-let shuffledArray = tempArray.sort((a, b) => 0.5 - Math.random());
-// console.log(shuffledArray)
+    await newItems()
+    attempts += 3
+    document.getElementById('attempts').innerHTML = attempts
 
-for (let i = 0; i < rangeVar.length; i++){
-    board.innerHTML += shuffledArray[i]
+    score += 10
+    document.getElementById('score').innerHTML = score
+
+    clearnUpHiddenDivs()
+
+    shuffledArray = tempArray.sort((a, b) => 0.5 - Math.random());
+    for (let i = 0; i < [...Array(x).keys()].length; i++){
+        board.innerHTML += shuffledArray[i]
+    }
+    console.log(matcherPerRound , 'a')
+    console.log([...Array(x).keys()].length, 'b')
 }
-
-console.log(board)
-
-cardImages = {
-    1: '#85e3ff',
-    2: '#aff8d8',
-    3: '#ffb5e8',
-    4: '#ffabab',
-    5: '#f67280',
-    0: '#6b7b8e'
+function newItems() {
+    // alert(1)
+    counter += 1
+    tempString = '<div class="cards" id="'+counter+'" onClick="showId()"></div>'
+    tempArray.push(tempString)
+    tempString = '<div class="cards" id="'+counter+'" onClick="showId()"></div>'
+    tempArray.push(tempString)
 }
-
 
 
 function showId(){
     clickCounter += 1
-
-        if (clickCounter == 1){
-            card1 = window.event.target.id
     
-            dom1 = window.event.target
-            dom1.style.pointerEvents = 'none';
-
-            dom1.style.backgroundColor = cardImages[card1]
-
-            console.log('card1:',card1)
-        }
-        else if (clickCounter == 2){
-            card2 = window.event.target.id
-            dom2 = window.event.target
-            dom2.style.backgroundColor = cardImages[card2]
-            console.log('card2:', card2)
-           
-            matchTest()
-            document.getElementById('pausedScreen').style.display = 'block'
-            dom2.style.pointerEvents = 'none';
-            dom1.style.pointerEvents = 'none';
-            clickCounter = 0
-
+    if (clickCounter == 1){
+        card1 = window.event.target.id
         
+        dom1 = window.event.target
+        dom1.style.pointerEvents = 'none';
+        
+        dom1.style.backgroundColor = cardImages[card1]
+        
+        console.log('card1:',card1)
+    }
+    else if (clickCounter == 2){
+        card2 = window.event.target.id
+        dom2 = window.event.target
+        dom2.style.backgroundColor = cardImages[card2]
+        // console.log('card2:', card2)
+        
+        matchTest()
+        document.getElementById('pausedScreen').style.display = 'block'
+        dom2.style.pointerEvents = 'none';
+        dom1.style.pointerEvents = 'none';
+        clickCounter = 0
     }
 }
 
 function matchTest() {
+    
+    matchCounter = 0
     console.log('card1:', card1, 'card2:', card2)
     if (card1 == card2){
+        score += 5
+        document.getElementById('score').innerHTML = score
         document.getElementById('board').style.pointerEvents = 'none'
-        setTimeout(showMatch, 2000)
-   
+        setTimeout(showMatch, 1000)
+        matcherPerRound +=2
+        console.log(matcherPerRound , 'a')
+        console.log([...Array(x).keys()].length, 'b')
+        if (matcherPerRound == [...Array(x).keys()].length ){
+            dom1.style.display = 'none'
+            dom2.style.display = 'none'
+       
+            x += 2
+            
+            matcherPerRound = 0
+    
+            dom1.style.backgroundColor = 'palegoldenrod'
+            dom2.style.backgroundColor = 'palegoldenrod'
+            dom1.style.pointerEvents = 'auto';
+            dom2.style.pointerEvents = 'auto';
+            
+            document.getElementById('attempts').innerHTML = attempts
+            addMoreItems()
+        }
     }
     else{
         document.getElementById('board').style.pointerEvents = 'none'
-        setTimeout(resetCard, 2000)
-    
+        setTimeout(notAMatch, 1000)
     }
-
-
 }
 function showMatch() {
-    dom1.style.display = 'none'
-    dom2.style.display = 'none'
+    dom1.style.visibility  = 'hidden'
+    dom2.style.visibility  = 'hidden'
+    
     document.getElementById('board').style.pointerEvents = 'auto'
     document.getElementById('pausedScreen').style.display = 'none'
-
+    
 }
 
-function resetCard() {
+function notAMatch() {
     dom1.style.backgroundColor = 'palegoldenrod'
     dom2.style.backgroundColor = 'palegoldenrod'
     document.getElementById('board').style.pointerEvents = 'auto'
     dom1.style.pointerEvents = 'auto';
     dom2.style.pointerEvents = 'auto';
     document.getElementById('pausedScreen').style.display = 'none'
+    
+    attempts -= 1
+    document.getElementById('attempts').innerHTML = attempts
+
+    if (attempts == 0){
+
+
+        let endingArray = document.querySelectorAll('.cards')
+
+        endingArray.forEach(item => {
+            item.style.pointerEvents = 'none'
+            item.style.backgroundColor = cardImages[item.id]
+       
+        })
+    }
 }
 
+
+document.getElementById('attempts').innerHTML = attempts
+generateGame()
